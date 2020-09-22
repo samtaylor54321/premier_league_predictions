75b52a1e2aa5a0963783ffb003f41c094eedda5e
Sam Taylor <sam@MacBook-Pro.local>
2020-09-22 22:31:45 +0100

    refactored code to make the parsing of goals tider

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 from glob import glob
 from src.predictor import Predictor
-from src.cleaner import Cleaner, parse_promoted_away_team_goals, parse_promoted_home_team_goals
+from src.cleaner import Cleaner
 from ruamel_yaml import safe_load
 
 
 def main():
     # Instantiate empty dataset for all results
-    away_goals_dataset = {}
-    home_goals_dataset = {}
+    # TODO Make this into one dictionary rather than several
+    away_goals_scored_dataset = {}
+    away_goals_conceded_dataset = {}
+    home_goals_scored_dataset = {}
+    home_goals_conceded_dataset = {}
 
     with open("config.yml", "r") as file:
         config = safe_load(file)
@@ -20,56 +23,97 @@ def main():
         # Instantiate cleaner object
         cleaner = Cleaner(dataset)
 
-        # Parse away goals and append to database
-        away_goals_dict = cleaner.generate_away_goals()
-
-        away_goals_dataset.update(away_goals_dict)
+        # Parse away goals
+        away_goals_scored, away_goals_conceded = cleaner.generate_away_goals()
+        # Append away goals to dataset
+        away_goals_scored_dataset.update(away_goals_scored)
+        away_goals_conceded_dataset.update(away_goals_conceded)
 
         # Parse home goals and append to database
-        home_goals_dict = cleaner.generate_home_goals()
+        home_goals_scored, home_goals_conceded = cleaner.generate_home_goals()
+
+        # TODO tidy this section so it only get's called once
 
         # Make the that these have both got the same keys
-        new_home_goals = {}
-        for i, value in enumerate(away_goals_dict.keys()):
-            new_home_goals[value] = home_goals_dict[i]
+        new_home_goals_scored = {}
+        for i, value in enumerate(away_goals_scored.keys()):
+            new_home_goals_scored[value] = home_goals_scored[i]
 
-        home_goals_dataset.update(new_home_goals)
+        home_goals_scored_dataset.update(new_home_goals_scored)
 
-        # Update promoted team home and away goals
-        for abb, team, dataset in zip(["LEE", "FUL", "WBA"], ["Leeds United", "Fulham", "West Bromwich Albion"],
+        # # Make the that these have both got the same keys
+        new_home_goals_conceded = {}
+        for i, value in enumerate(away_goals_conceded.keys()):
+            new_home_goals_conceded[value] = home_goals_conceded[i]
+
+        home_goals_conceded_dataset.update(new_home_goals_conceded)
+
+    # Loop through prompted teams
+    for abb, team, filepath in zip(["LEE", "FUL", "WBA"], ["Leeds United", "Fulham", "West Bromwich Albion"],
                                  ["data/premier_league_2003_2004.csv", "data/premier_league_results_2018_2019.csv",
                                   "data/premier_league_results_2017_2018.csv"]):
+        # TODO Tidy this section so it's a bit neater
+        # Read the dataset
+        with open(filepath) as file:
+            dataset = pd.read_csv(file)
+            # Parse away goals from the given dataset
+            prompted_team_away_goals_scored, prompted_team_away_goals_conceded = \
+                cleaner.generate_promoted_team_away_goals(abb, dataset)
+            # Update away goals in original dataset
+            away_goals_scored_dataset[abb] = prompted_team_away_goals_scored
+            away_goals_conceded_dataset[abb] = prompted_team_away_goals_conceded
 
-            away_goals_dataset[abb] = parse_promoted_away_team_goals(abb, dataset)
-            home_goals_dataset[abb] = parse_promoted_home_team_goals(team, dataset)
+            # Parse away goals from the given dataset
+            prompted_team_home_goals_scored, prompted_team_home_goals_conceded = \
+                cleaner.generate_promoted_team_home_goals(team, dataset)
+            # Update away goals in original dataset
+            home_goals_scored_dataset[abb] = prompted_team_home_goals_scored
+            home_goals_conceded_dataset[abb] = prompted_team_home_goals_conceded
 
-    # # Use new datasets to update results for the current campaing.
+    # Use new datasets to update results for the current campaing.
     for file in glob("data/*_new_season.csv"):
         with open(file) as dataset:
             dataset = pd.read_csv(dataset)
             cleaner = Cleaner(dataset)
-
+            # TODO tidy this section so it's less repetative
             # Parse new seasons away goals
-            away_goals_new_season = cleaner.generate_away_goals()
+            away_goals_scored_new, away_goals_conceded_new = cleaner.generate_away_goals()
 
-            # Update away goals dictionary with result from the new season
-            for key, values in away_goals_new_season.items():
-                away_goals_dataset[key] = np.append(away_goals_dataset[key], away_goals_new_season[key])
+            # Update away goals scored dictionary with result from the new season
+            for key, values in away_goals_scored_new.items():
+                away_goals_scored_dataset[key] = np.append(away_goals_scored_dataset[key],
+                                                           away_goals_scored_new[key])
 
-            # Parse new seasons home goals
-            home_goals_new_season = cleaner.generate_home_goals()
+            # Update away goals scored dictionary with result from the new season
+            for key, values in away_goals_conceded_new.items():
+                away_goals_conceded_dataset[key] = np.append(away_goals_conceded_dataset[key],
+                                                             away_goals_conceded_new[key])
+
+            # Parse new seasons home scored goals
+            home_goals_scored_new_season, home_goals_conceded_new_season = cleaner.generate_home_goals()
 
             # Update dictionary keys
-            new_season_home_goals = {}
-            for i, value in enumerate(away_goals_new_season.keys()):
-                new_season_home_goals[value] = home_goals_new_season[i]
+            home_goals_scored_dataset_update = {}
+            for i, value in enumerate(away_goals_scored_new.keys()):
+                home_goals_scored_dataset_update[value] = home_goals_scored_new_season[i]
 
             # Update home goals dictionary with results from the new season
-            for key, values in new_season_home_goals.items():
-                home_goals_dataset[key] = np.append(home_goals_dataset[key], new_season_home_goals[key])
+            for key, values in home_goals_scored_dataset_update.items():
+                home_goals_scored_dataset[key] = np.append(home_goals_scored_dataset[key],
+                                                           home_goals_scored_dataset_update[key])
+
+            # Update dictionary keys
+            home_goals_conceded_dataset_update = {}
+            for i, value in enumerate(away_goals_conceded_new.keys()):
+                home_goals_conceded_dataset_update[value] = home_goals_conceded_new_season[i]
+
+            # Update home goals dictionary with results from the new season
+            for key, values in home_goals_conceded_dataset_update.items():
+                home_goals_conceded_dataset[key] = np.append(home_goals_conceded_dataset[key],
+                                                             home_goals_conceded_dataset_update[key])
 
     # Create predictor object
-    predictor = Predictor(config, home_goals_dataset, away_goals_dataset)
+    predictor = Predictor(config, home_goals_scored_dataset, away_goals_scored_dataset)
 
     # Print predictions to screen
     predictor.make_predictions()
--- src/cleaner.py
+++ src/cleaner.py
@@ -10,138 +10,135 @@ class Cleaner:
             dataset (pd.DataFrame): dataset containing the result for a given league in a given season in matrix form.
                 An example is available at https://en.wikipedia.org/wiki/2019%E2%80%9320_EFL_Championship#Results
         """
-        self.dataset = dataset
+        self.dataset = self._parse_bad_characters(dataset)
         self.dataset = self.dataset.set_index("Home \ Away")
-
-    def generate_away_goals(self):
-        """Produces a dictionary which contains the away goals for a given dataset
-
-        Return:
-            dict: a dictionary of array which represents the number of away goals scored by each team in a given season
-        """
-        # Instantiate an empty dictionary to be populated
-        away_goals_dict = {}
-
-        # Generate away goals for each team in the dataset
-        for col in self.dataset.columns:
-            self.dataset.loc[self.dataset[col].isna(), col] = "-"
-            away_goals = self.dataset[col].str[2:]
-
-            # Check which value is null and remove
-            array_filter = (away_goals != "")
-            away_goals_dict[col] = away_goals.array[array_filter].astype(int)
-
-        return away_goals_dict
 
     def generate_home_goals(self):
         """Produces a dictionary which contains the home goals for a given dataset
 
         Return:
-            dict: a dictionary of array which represents the number of home goals scored by each team in a given season
+            tuple: a tuple of arrays which represents the number of home goals scored and conceded
+            by each team in a given season
         """
-        # Instantiate an empty dictionary to be populated
-        home_goals_dict = {}
+        # Instantiate empty dictionaries
+        home_goals_scored = {}
+        home_goals_conceded = {}
 
-        # Outer loop calculates the result for each team in the dataset
+        # Loop through dataset
         for row in range(self.dataset.shape[0]):
-            home_scores = []
-            row = row
-            # Inner loop calculates the result for each opposition team faced
-            for col in range(self.dataset.shape[1]):
-                home_scores.append(self.dataset.iloc[row, col][0])
-            # Summary of the goals scored by the team is appended to the dictionary
-            home_goals_dict[row] = np.asarray(home_scores)
+            # Excluding missing values
+            mask = ~self.dataset.iloc[row].isnull().values
+            # Append goals scored at home
+            home_goals_scored[row] = self.dataset.iloc[row][mask].str[0].values.astype(int)
+            # Append goals conceded at home
+            home_goals_conceded[row] = self.dataset.iloc[row][mask].str[2].values.astype(int)
 
-        # Address non-numeric contains contained within the dataset
-        for i in home_goals_dict.keys():
-            # Remove '�'
-            mask_one = home_goals_dict[i] != '�'
-            home_goals_dict[i] = home_goals_dict[i][mask_one]
+        return home_goals_scored, home_goals_conceded
 
-            # Remove 'a'
-            mask_one = home_goals_dict[i] != 'a'
-            home_goals_dict[i] = home_goals_dict[i][mask_one]
+    def generate_away_goals(self):
+        """Produces a dictionary which contains the away goals for a given dataset
 
-            # Remove "-"
-            mask_two = home_goals_dict[i] != '-'
-            home_goals_dict[i] = home_goals_dict[i][mask_two]
+        Return:
+            tuple: a tuple of arrays which represents the number of away goals scored and conceded
+            by each team in a given season
+        """
+        # Instantiate empty dictionaries
+        away_goals_scored = {}
+        away_goals_conceded = {}
 
-            # Remove "-"
-            mask_three = home_goals_dict[i] != ' '
-            home_goals_dict[i] = home_goals_dict[i][mask_three]
+        # Loop through dataset
+        for col in self.dataset.columns:
+            # Exclude missing values
+            mask = ~self.dataset[col].isnull().values
+            # Append goals scored away
+            away_goals_scored[col] = self.dataset[col][mask].str[2].values.astype(int)
+            # Append goals conceded away
+            away_goals_conceded[col] = self.dataset[col][mask].str[0].values.astype(int)
 
-            # Convert array to an int type
-            home_goals_dict[i] = home_goals_dict[i].astype(int)
+        return away_goals_scored, away_goals_conceded
 
-        return home_goals_dict
+    def generate_promoted_team_away_goals(self, promoted_team, dataset):
+        """Overwrites existing values for teams which have been prompted to the premier league this season
+
+        Args:
+            promoted_team (str): 3 letter abb for the prompted team
+            dataset (DataFrame): DataFrame of result for the team's last outing in the premier league.
+
+        Return:
+            tuple: a tuple of arrays which represents the number of away goals scored and conceded
+            by each team in a given season
+        """
+        # Remove bad characters from dataset
+        dataset = self._parse_bad_characters(dataset)
+
+        # Set team name as index
+        try:
+            dataset = dataset.set_index("Home \ Away")
+        except KeyError:
+            print("Index column 'Home \ Away' not present in dataset")
+
+        # Loop through the dataframe until the correct column is reached
+        for col in dataset.columns:
+            # Ignore the column if it's not the prompted team in question
+            if col != promoted_team:
+                pass
+            else:
+                # Exclude missing values
+                mask = ~dataset[col].isnull().values
+                # Append goals scored away
+                away_goals_scored = dataset[col][mask].str[2].values.astype(int)
+                # Append goals conceded away
+                away_goals_conceded = dataset[col][mask].str[0].values.astype(int)
+
+                return away_goals_scored, away_goals_conceded
+
+    def generate_promoted_team_home_goals(self, promoted_team, dataset):
+        """Overwrites existing values for teams which have been prompted to the premier league this season
+
+        Args:
+            promoted_team (str): Name of team to replace in index
+            filepath (str): Filepath to result for the team's last outing in the premier league.
+
+        Return:
+            tuple: a tuple of arrays which represents the number of home goals scored and conceded
+            by each team in a given season
+        """
+        # Remove bad characters from dataset
+        dataset = self._parse_bad_characters(dataset)
+
+        # Set team name as index
+        try:
+            dataset = dataset.set_index("Home \ Away")
+        except KeyError:
+            print("Index column 'Home \ Away' not present in dataset")
+
+        # Get position of promoted team in index
+        team_index_value = dataset.index.get_loc(promoted_team)
+        # Exclude missing values
+        mask = ~dataset.iloc[team_index_value].isnull().values
+        # Append goals scored at home
+        home_goals_scored = dataset.iloc[team_index_value][mask].str[0].values.astype(int)
+        # Append goals conceded at home
+        home_goals_conceded = dataset.iloc[team_index_value][mask].str[2].values.astype(int)
+
+        return home_goals_scored, home_goals_conceded
+
+    def _parse_bad_characters(self, dataset):
+        """Removes bad characters from the dataset
+
+        Notes:
+            By default, this method will replace bad characters with np.nan
+
+        Returns:
+            DataFrame: DataFrame with bad characters replaced
+        """
+        # Pass dataset to class
+        self.dataset = dataset
+        # Loop through known bad characters
+        for character in ["a", "�", "-", " "]:
+            # Remove this and replace with NaN
+            self.dataset = self.dataset.replace(character, np.nan)
+
+        return self.dataset
 
 
-def parse_promoted_away_team_goals(promoted_team, filepath):
-    """Overwrites existing values for teams which have been prompted to the premier league tihs season
-
-    Notes:
-        Teams promoted to the Premier League for this season will likely have excelled in the Championship and
-        will struggle more in the premier league. Given this, goals for these teams will be replaced with values
-        from previous season in the premier league.
-
-    Args:
-        promoted_team (str): 3 letter abb for the prompted team
-        filepath (str): Filepath to result for the team's last outing in the premier league.
-
-    Returns:
-        array: Away goals scored by the promoted team.
-    """
-    # Load file into memory
-    with open(filepath, "r") as file:
-        new_dataset = pd.read_csv(file)
-
-    # Set team name as index
-    new_dataset = new_dataset.set_index("Home \ Away")
-
-    # Loop through the dataframe until the correct column is reached
-    for col in new_dataset.columns:
-        if col != promoted_team:
-            pass
-        else:
-            # Extract the values of interest
-            away_goals = new_dataset[col].str[2:]
-            array_filter = np.logical_and(~away_goals.isnull(), away_goals != "")
-            promoted_team_goals = away_goals.array[array_filter].astype(int)
-
-            return promoted_team_goals
-
-
-def parse_promoted_home_team_goals(promoted_team, filepath):
-    """Overwrites existing values for teams which have been prompted to the premier league this season
-
-    Notes:
-        Teams promoted to the Premier League for this season will likely have excelled in the Championship and
-        will struggle more in the premier league. Given this, goals for these teams will be replaced with values
-        from previous season in the premier league.
-
-    Args:
-        promoted_team (str): Name of team to replace in index
-        filepath (str): Filepath to result for the team's last outing in the premier league.
-
-    Returns:
-        array: Home goals scored by the promoted team.
-    """
-    # Load file into memory
-    with open(filepath, "r") as file:
-        new_dataset = pd.read_csv(file)
-
-    # Set team name as index
-    new_dataset = new_dataset.set_index("Home \ Away")
-
-    # Get position of promoted team in index
-    team_index_value = new_dataset.index.get_loc(promoted_team)
-    home_scores = []
-
-    # Loop through the dataset extracting the number of home goals scored
-    for col in range(new_dataset.shape[1]):
-        home_scores.append(new_dataset.iloc[team_index_value, col][0])
-
-    home_scores = np.asarray(home_scores)
-    # Remove any strange characters which appear
-    mask = np.logical_and(home_scores != '-', home_scores != '�')
-    return home_scores[mask].astype(int)
